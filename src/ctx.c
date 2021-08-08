8dbc2457d224a4b8c0193d74c377d756096b43d0
Michael Drake <tlsa@netsurf-browser.org>
2021-08-08 17:30:11 +0100

    Paragraph: Add module for handling library context.

---
+++ src/ctx.c
@@ -0,0 +1,49 @@
+/*
+ * SPDX-License-Identifier: ISC
+ *
+ * Copyright (C) 2019 Michael Drake <tlsa@netsurf-browser.org>
+ */
+
+/**
+ * \file
+ * \brief Paragraph context handling.
+ */
+
+#include <stdlib.h>
+
+#include <paragraph.h>
+
+#include "ctx.h"
+
+/* Exported function, documented in `include/paragraph.h` */
+paragraph_err_t paragraph_ctx_create(
+		void *pw,
+		paragraph_ctx_t **ctx_out,
+		const paragraph_cb_text_t *cb_text)
+{
+	paragraph_ctx_t *ctx;
+
+	if (ctx_out == NULL || cb_text == NULL) {
+		return PARAGRAPH_ERR_BAD_PARAM;
+	}
+
+	ctx = calloc(1, sizeof(*ctx));
+	if (ctx == NULL) {
+		return PARAGRAPH_ERR_OOM;
+	}
+
+	ctx->pw = pw;
+	ctx->cb_text = cb_text;
+
+	*ctx_out = ctx;
+	return PARAGRAPH_OK;
+}
+
+/* Exported function, documented in `include/paragraph.h` */
+paragraph_ctx_t *paragraph_ctx_destroy(
+		paragraph_ctx_t *ctx)
+{
+	free(ctx);
+
+	return NULL;
+}
---
+++ src/ctx.h
@@ -0,0 +1,20 @@
+/*
+ * SPDX-License-Identifier: ISC
+ *
+ * Copyright (C) 2021 Michael Drake <tlsa@netsurf-browser.org>
+ */
+
+/**
+ * \file
+ * \brief Paragraph content interface.
+ */
+
+#ifndef PARAGRAPH__CTX_H
+#define PARAGRAPH__CTX_H
+
+struct paragraph_ctx_s {
+	void *pw;
+	const paragraph_cb_text_t *cb_text;
+};
+
+#endif
